caa991fd691410b4319e537d236ac36f97766b03
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:37:42 -0300

    Add files via upload

---
+++ Lista2Ex10/Lista2Ex10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o peso da pessoa em kg:");
+        double peso = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a altura da pessoa em metros:");
+        double altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o sexo da pessoa (M para masculino, F para feminino):");
+        char sexo = char.Parse(Console.ReadLine().ToUpper());
+
+        double imc = CalcularIMC(peso, altura);
+
+        Console.WriteLine($"O IMC da pessoa é: {imc}");
+
+        if (sexo == 'M')
+        {
+            if (imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc >= 20 && imc < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else
+            {
---
+++ Lista2Ex11/Lista2Ex11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a nota da primeira avaliação (P1):");
+        double notaP1 = double.Parse(Console.ReadLine());
+
+        double mediaDesejada = 5;
+
+        double notaP2Necessaria = CalcularNotaP2Necessaria(notaP1, mediaDesejada);
+
+        Console.WriteLine($"Para ser aprovado, você precisa tirar pelo menos {notaP2Necessaria} na segunda avaliação (P2).");
+    }
+
+    static double CalcularNotaP2Necessaria(double notaP1, double mediaDesejada)
+    {
+        return (mediaDesejada * 2) - notaP1;
+    }
+}
