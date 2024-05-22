3de7a16f86f691d46b292d3ffbed2d67ceca97bb
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:27:44 -0300

    Add files via upload

---
+++ Lista2Ex6/Lista2Ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex6
+{
+    internal class Program
+      
+    {
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Digite o peso da pessoa (em kg): ");
+                double peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a altura da pessoa (em metros): ");
+                double altura = Convert.ToDouble(Console.ReadLine());
+
+              
+                double imc = peso / (altura * altura);
+
+                
+                if (imc < 20)
+                    Console.WriteLine("Abaixo do peso");
+                else if (imc < 25)
+                    Console.WriteLine("Peso ideal");
+                else
+                    Console.WriteLine("Acima do peso");
+            }
+        }
+    }
+
+}
+
+
