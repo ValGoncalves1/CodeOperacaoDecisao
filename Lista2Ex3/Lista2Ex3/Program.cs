876f7f8fb8f8703a0697527d191740ca4b280c42
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:26:51 -0300

    Add files via upload

---
+++ Lista2Ex3/Lista2Ex3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.Write("Digite o primeiro valor ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o terceiro valor");
+            c = int.Parse(Console.ReadLine());
+
+            if (a == b)
+                if (a == c)
+                    Console.WriteLine("Iguais ");
+                else
+                    if (a > c)
+                    Console.WriteLine("Primeiro/Segundo ");
+                else
+                    Console.WriteLine("Terceiro");
+
+            else
+
+            if (a == c)
+                if (a > b)
+                    Console.WriteLine("Primeiro e Terceiro  ");
+                else
+                    Console.WriteLine("Segundo ");
+
+            else
+
+                 if (b == c)
+                if (b > a)
+                    Console.WriteLine("Segundo / Terceiro ");
+                else
+                    Console.WriteLine("Primeiro ");
+
+            else
+                if (a > b)
+                if (a > c)
+                    Console.WriteLine("Primeiro ");
+                else
+
+                    Console.WriteLine("Terceiro ");
+
+            else
+                     if (b > c)
+
+                Console.WriteLine("Segundo ");
+            else
+                Console.WriteLine("Terceiro ");
+
+                
+        }
+    }
+}
---
+++ Lista2Ex5/Lista2Ex6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int base1;
+            int altura;
+            int área;
+
+            Console.WriteLine("Digite o valor da área do retângulo:");
+            base1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura do retangulo:");
+            altura = int.Parse(Console.ReadLine());
+
+            área = base1 * altura;
+
+            if (área > 100)
+                Console.WriteLine("Terreno grande.");
+            else
+                Console.WriteLine("Terreno pequeno:");
+        }
+    }
+}
